1ddbe26b0ff41d22850ffeb2d33a67b2ca43cb3f
719676340 <719676340@qq.com>
2021-04-09 20:34:28 +0800

    访客模块的布局完成

---
+++ src/components/addvisitor.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class=''>
+      <el-dialog title="新增访客信息" v-model="visible" width="400px" ref="infoform">
+        <el-form :model="info">
+            <el-form-item label="姓名" prop="name">
+                <el-input  v-model="info.name"></el-input>
+            </el-form-item>
+            <el-form-item label="身份证号" prop="name">
+                <el-input  v-model="info.idcart"></el-input>
+            </el-form-item>
+            <el-form-item label="拜访目的" prop="name">
+                <el-input  v-model="info.content"></el-input>
+            </el-form-item>
+        </el-form>
+        <template #footer>
+          <span class="dialog-footer">
+            <el-button type="danger" @click="close"><span class="bottonword">取消</span></el-button>
+            <el-button type="primary"><span class="bottonword">确 定</span></el-button>
+          </span>
+        </template>
+      </el-dialog>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, toRefs } from 'vue'
+export default {
+  name: 'addvisitor',
+  setup(){
+      const infoform=ref(null)
+      const state=reactive({
+          visible:false,
+          info:{
+              name:'',
+              idcart:'',
+              content:''
+          }
+      })
+      const open=function(){
+          state.visible=true
+          state.info=reactive({
+              name:'',
+              idcart:'',
+              content:''
+          })
+      }
+      const close=function(){
+          state.visible=false
+      }
+
+    
+      return{
+          ...toRefs(state),
+          infoform,
+          open,
+          close
+      }
+  }
+}
+</script>
+<style>
+.bottonword{
+    color: #fff;
+}   
+</style>
--- src/views/visitor.vue
+++ src/views/visitor.vue
@@ -1,25 +1,98 @@
 <!-- 访客登记表 两个部分 一个是信息的登记form 改成按钮弹出小框登记的  一个是信息的展示table
   -->
 <template>
-    <el-card class="container">
+    <el-card class="container" shadow="always">
         <template #header>
-          <div class="card-header">
-            <el-button class="button" type="text">操作按钮</el-button>
-            <el-button class="button" type="text">操作按钮</el-button>
+          <div class="card-header clearfix">
+            <el-button class="button cardbotton" type="primary" @click="addvisitor"><span class="bottonword">新增访客</span></el-button>
           </div>    
         </template>
+        <div class="contenttable">
+            <el-table :data="tableData" style="width: 100%">
+                <el-table-column prop="date" label="时间"></el-table-column>
+                <el-table-column prop="name" label="名字"></el-table-column>
+                <el-table-column prop="address" label="地址"></el-table-column>
+                <!-- <el-table-column prop="date" label="时间"></el-table-column> -->
+                <el-table-column fixed="right" label="操作" width="120">
+                    <template #default>
+                      <el-button
+                        type="danger"   
+                        size="small"
+                        @click="sayhi"
+                        >
+                        <span class="bottonword">移除</span>
+                      </el-button>
+                      <!-- <el-button class="button cardbotton" type="primary" >新增访客</el-button> -->
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
     </el-card>
+    <addvisitor ref="add"></addvisitor>
 </template>
 
 <script>
+import { reactive, toRefs ,ref} from 'vue'
+import addvisitor from '../components/addvisitor'
 export default {
   name: 'visitor',
+  components:{
+    addvisitor
+  },
+  setup(){
+      const add=ref(null)
+      const state=reactive({
+        tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
+      })
+      const sayhi=function(){
+          console.log('hello')
+      }
+      const addvisitor=function(){
+          add.value.open()
+      }
+      return {
+          ...toRefs(state),
+          add,
+          sayhi,
+          addvisitor
+      }
+  }
 }
 </script>
 <style>
+.clearfix::before,.clearfix::after{
+    display: table;
+    content: ' ';
+    clear: both;
+}
 .container{
+    position: relative;
+    overflow: hidden;
 }
-.card-header{
-    
+.cardbotton{
+    float: left;
 }
+.bottonword{
+    color: #fff;
+}   
 </style>
